e30360c7b7121a032d7ba57f3d6a36c690a0084f
Andrei Parfenov <andreymail98@gmail.com>
2021-10-22 16:19:30 +0300

    first commit

---
+++ react-2/1.context.tasks/build/precache-manifest.990447ec3099df8dce89ead4282e2f72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9ab1ec33cdfa5fa5a2ec977063a66e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1f7c289629e322e2bac",
+    "url": "/static/css/main.3ad6b746.chunk.css"
+  },
+  {
+    "revision": "d9e18ea2178082f14cff",
+    "url": "/static/js/2.c87e3013.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c87e3013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f7c289629e322e2bac",
+    "url": "/static/js/main.343406f4.chunk.js"
+  },
+  {
+    "revision": "c1e1abe6d308faacc7d0",
+    "url": "/static/js/runtime-main.2c854d87.js"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "214066000eefee88185c190ee74810df",
+    "url": "/static/media/Robotocondensed.21406600.woff"
+  },
+  {
+    "revision": "9c64c02a0b1e5e9ebea64953200dd7b1",
+    "url": "/static/media/bed.9c64c02a.svg"
+  }
+]);
